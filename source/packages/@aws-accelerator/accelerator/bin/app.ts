214c0412376071b460043513ff942afc90de466d
GopiRadhi <113072462+GopiRadhi@users.noreply.github.com>
2022-11-29 09:40:50 -0800

    Update app.ts

--- source/packages/@aws-accelerator/accelerator/bin/app.ts
+++ source/packages/@aws-accelerator/accelerator/bin/app.ts
@@ -117,7 +117,7 @@ async function main() {
     const region = app.node.tryGetContext('region');
     const partition = app.node.tryGetContext('partition');
 
-    let globalRegion = 'ap-southeast-2';
+    let globalRegion = 'ca-central-1';
 
     if (partition === 'aws-us-gov') {
       cdk.Aspects.of(app).add(new GovCloudOverrides());
